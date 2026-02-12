8506afb05fcedcda4047d15142ee04665fa50109
Neo Ferrer Mohedano <118294424+Elneocapo@users.noreply.github.com>
2026-02-12 03:05:08 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,35 +1,65 @@
 //IMPORTAR LIBRERIA
+
 import { WebSocketServer, WebSocket } from "ws";
 
+
+
 //creamos servidor y le decimos que escuche al puerto 3000
-const servidor = new WebSocketServer({ port: process.env.PORT || 3000 });
+
+const servidor = new WebSocketServer({ port: 3000 });
+
+
 
 //funcion que se ejecuta cuando el cliente se conecta
+
 servidor.on("connection", (ws) => {
-  console.log("cliente conectado");
 
-  //el servidor manda mensaje SOLO al cliente que se acaba de conectar (no a todos)
-  ws.send("Bienvenido 👋");
+  console.log("cliente conectado");
 
-  //cuando este cliente me mande algo, avísame
-  ws.on("message", (message) => {
-    let datos_recibidos = message;
 
-    //Miraver si el mensaje es para el servidor o no
-    if (datos_recibidos.includes("ALLCLIENTS_LOG")) {
-      servidor.clients.forEach((client) => {
-        if (client.readyState === 1) {
-          //el JSON mierda ese es para que se mande como array
-          client.send(
-            JSON.stringify([
-              datos_recibidos[0],
-              datos_recibidos[1].toString().replace("ALLCLIENTS_LOG", "").trim(),
-            ])
-          );
-        }
-      });
-    } else {
-      console.log(datos_recibidos);
-    }
-  }); // <--- Faltaba cerrar el ws.on("message")
-}); // <--- Faltaba cerrar el servidor.on("connection")
+  //el servidor manda mensaje SOLO al cliente que se acaba de conectar (no a todos)
+
+  ws.send("Bienvenido 👋");
+
+
+
+
+  //cuando este cliente me mande algo, avísame
+
+    ws.on("message", (message) => {
+
+
+
+    let datos_recibidos = message;
+
+
+
+    //Miraver si el mensaje es para el servidor o no
+
+    if(datos_recibidos.includes("ALLCLIENTS_LOG")){
+
+
+
+        servidor.clients.forEach((client) => {
+
+                if (client.readyState === 1) {
+
+                                //el JSON mierda ese es para que se mande como array
+
+                    client.send(JSON.stringify([datos_recibidos[0], datos_recibidos[1].toString().replace("ALLCLIENTS_LOG", "").trim()]));
+
+                }
+
+        });
+
+
+
+    }else{
+
+        console.log(datos_recibidos);
+
+    }
+
+
+  });
+});
