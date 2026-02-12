9889a8cc370aa3e1ccc12b325aea0f8102370843
Neo Ferrer Mohedano <118294424+Elneocapo@users.noreply.github.com>
2026-02-12 03:13:14 +0100

    Update server.js

--- server.js
+++ server.js
@@ -6,60 +6,59 @@ import { WebSocketServer, WebSocket } from "ws";
 
 //creamos servidor y le decimos que escuche al puerto 3000
 
-const servidor = new WebSocketServer({ port: 3000 });
-
+const PORT = process.env.PORT || 3000;
+const servidor = new WebSocketServer({ port: PORT });
 
 
 //funcion que se ejecuta cuando el cliente se conecta
 
 servidor.on("connection", (ws) => {
 
-  console.log("cliente conectado");
+console.log("cliente conectado");
 
 
-  //el servidor manda mensaje SOLO al cliente que se acaba de conectar (no a todos)
+//el servidor manda mensaje SOLO al cliente que se acaba de conectar (no a todos)
 
-  ws.send("Bienvenido 👋");
-
-
+ws.send("Bienvenido 👋");
 
 
-  //cuando este cliente me mande algo, avísame
+
+
+//cuando este cliente me mande algo, avísame
 
     ws.on("message", (message) => {
 
 
 
-    let datos_recibidos = message;
+ let datos_recibidos = message;
 
 
 
-    //Miraver si el mensaje es para el servidor o no
+ //Miraver si el mensaje es para el servidor o no
 
-    if(datos_recibidos.includes("ALLCLIENTS_LOG")){
-
-
-
-        servidor.clients.forEach((client) => {
-
-                if (client.readyState === 1) {
-
-                                //el JSON mierda ese es para que se mande como array
-
-                    client.send(JSON.stringify([datos_recibidos[0], datos_recibidos[1].toString().replace("ALLCLIENTS_LOG", "").trim()]));
-
-                }
-
-        });
+ if(datos_recibidos.includes("ALLCLIENTS_LOG")){
 
 
 
-    }else{
+ servidor.clients.forEach((client) => {
 
-        console.log(datos_recibidos);
+ if (client.readyState === 1) {
 
-    }
+        //el JSON mierda ese es para que se mande como array
+
+          client.send(JSON.stringify([datos_recibidos[0], datos_recibidos[1].toString().replace("ALLCLIENTS_LOG", "").trim()]));
+
+       }
+
+   });
 
 
-  });
+
+  }else{
+   console.log(datos_recibidos);
+
+  }
+
+
+ });
 });
